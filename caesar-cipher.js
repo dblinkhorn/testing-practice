3c3af15b854fd45d5d7519ec064e0e1fb242690a
dblinkhorn <dblinkhorn@gmail.com>
2021-09-23 11:28:58 -0700

    added some comments and fixed some typos

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,19 +1,17 @@
 // encrypts a string using the caesar cipher
 
 const originalMessage = 'attack from the north at dawn';
-// const originalArray = Array.from(originalMessage);
 const key = 5;
-// let cipherNumberMessage = [];
-// let cipherArray = [];
-
-
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
 // array to store number values for each letter in alphabet
 const alphabetIndex = [];
+
 // populates alphabetIndex with number values
 for (let i = 1; i <= alphabet.length; i++) {
   alphabetIndex.push(i);
 }
+
 // object will store each letter as a property and its corresponding
 // numberical equivalent as the value
 const alphabetObject = {};
@@ -23,6 +21,7 @@ alphabet.forEach((letter, index) => {
   alphabetObject[letter] = index;
 })
 
+// makes an array from the message string
 function convertToArray(message) {
   message = message.toLowerCase();
   let messageArray = Array.from(message);
@@ -73,6 +72,7 @@ function convertToLetters(cipherNumberMessage) {
   return cipherMessage;
 }
 
+// returns enciphered original message
 function caesarCipher(message) {
   let messageArray = convertToArray(message);
   let numberMessage = convertToNumbers(messageArray);
